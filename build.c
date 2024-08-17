1f57a63c011c2e8138d6ac29837b528609826c0e
Jakub Boros <enginedevel@gmail.com>
2024-08-17 16:24:54 +0200

    Init.

---
+++ build.c
@@ -0,0 +1,31 @@
+#define BLD_IMPLEMENTATION
+#include "core/bld.h"
+
+#include "core/utils.h"
+
+i32
+main(i32 argc, char *argv[])
+{
+    BLD_TRY_REBUILD_SELF(argc, argv);
+    char *output = "program";
+
+    bld_sa_t cc = {0};
+
+    BLD_SA_PUSH(cc, "src/main.c");
+
+    BLD_SA_PUSH(cc, "-I.", "-Isrc", "-o", output, BLD_WARNINGS);
+
+    if (bld_contains("debug", argc, argv)) {
+        BLD_SA_PUSH(cc, "-D_DEBUG");
+    }
+
+    u32 res = bld_cc_params((const char **)cc.data, cc.count);
+    if (res != 0)
+        return res;
+
+    if (bld_contains("run", argc, argv)) {
+        return bld_run_program(output);
+    }
+
+    return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "core/utils.h"
+
+#include <stdio.h>
+
+i32
+main(void)
+{
+    printf("\\_/\n V\n");
+    return 0;
+}
